6ed429c514bcc1fda264bf54d453cda74fc3eec4
Aanand Kainth <aanand@akainth.me>
2020-06-07 14:56:25 -0700

    Start from scratch because certificate stopped working

--- Program.cs
+++ Program.cs
@@ -1,62 +1,72 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
-using System;
-using System.Linq;
-using System.Threading;
+﻿using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications; // Notifications library
 using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
+using System.Threading;
+using Windows.UI.Xaml;
+using Windows.Storage;
+using System;
 
-namespace EyeCare
+namespace EyeGuard
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            if (args.Any((arg) => arg.Contains("ToastActivated"))) Environment.Exit(0);
-
-            // Register COM server and activator type
-            DesktopNotificationManagerCompat.RegisterActivator<ClearingActivator>();
-            DesktopNotificationManagerCompat.RegisterAumidAndComServer<ClearingActivator>("Aanand.EyeCare");
-
-            // Construct the visuals of the toast (using Notifications library)
-            ToastContent toastContent = new ToastContent()
+            ToastVisual visual = new ToastVisual()
             {
-
-                Visual = new ToastVisual()
+                BindingGeneric = new ToastBindingGeneric()
                 {
-                    BindingGeneric = new ToastBindingGeneric()
+                    Children =
                     {
-                        Children =
-            {
-                new AdaptiveText()
-                {
-                    Text = "Give your eyes a break!"
-                },
-                new AdaptiveText()
-                {
-                    Text = "Refocus your eyes on something at least 20 feet away for 20 seconds"
-                },
-            }
+                        new AdaptiveText()
+                        {
+                            Text = "Give your eyes a break"
+                        },
+                        new AdaptiveProgressBar()
+                        {
+                            Title = "Focus on something 20 feet away",
+                            Value = new BindableProgressBarValue("progressValue"),
+                            ValueStringOverride = new BindableString("progressValueString"),
+                            Status = "Keep looking"
+                        }
                     }
                 }
             };
+            ToastContent content = new ToastContent()
+            {
+                Visual = visual
+            };
 
-            // Create the XML document (BE SURE TO REFERENCE WINDOWS.DATA.XML.DOM)
-            var doc = new XmlDocument();
-            doc.LoadXml(toastContent.GetContent());
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(content.GetContent());
 
             while (true)
             {
-                Thread.Sleep(20 * 59 * 1000);
+                Thread.Sleep(20 * 60 * 1000);
 
-                // And create the toast notification
-                var toast = new ToastNotification(doc);
+                ToastNotification toast = new ToastNotification(document);
+                toast.Tag = "refocus";
+                toast.Data = new NotificationData();
+                toast.Data.Values["progressValue"] = "0";
+                toast.Data.Values["progressValueString"] = "0/20 seconds";
 
-                // And then show it
-                DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
 
-                Thread.Sleep(20 * 1000);
-
-                DesktopNotificationManagerCompat.History.Clear();
+                for (uint seconds = 0; seconds <= 20; seconds++)
+                {
+                    var data = new NotificationData
+                    {
+                        SequenceNumber = seconds
+                    };
+                    data.Values["progressValue"] = (seconds / 20f).ToString();
+                    data.Values["progressValueString"] = $"{seconds}/20 seconds";
+                    if (ToastNotificationManager.CreateToastNotifier().Update(data, "refocus") != NotificationUpdateResult.Succeeded)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(1000);
+                }
+                ToastNotificationManager.History.Remove("refocus");
             }
         }
     }
